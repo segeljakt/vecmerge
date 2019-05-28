3f3d694fa219234e0f2bb758fc0ec6b3e6a74c0c
Klas Segeljakt <klasseg@kth.se>
2019-05-28 21:30:49 +0200

    Add ? operator, and pre-calculate capacity

--- README.md
+++ README.md
@@ -7,14 +7,36 @@ A macro for merging vectors.
 ```rust
 use vecmerge::vecmerge;
 
-fn main() {
-    assert_eq!(vec![1,2,3,4,5,6], vecmerge!([1,2,3] + [4,5] + [6]));
+assert_eq! {
+    vecmerge!([1, 2, 3] + [4, 5] + [6]),
+    vec![1, 2, 3, 4, 5, 6],
+}
 
-    let a = vec![1,2,3];
-    let b = vec![6];
+let a = vec![1, 2, 3];
+let b = vec![6];
 
-    assert_eq!(vec![1,2,3,4,5,6], vecmerge!(a + [4,5] + b));
+assert_eq! {
+    vecmerge!(a + [4, 5] + b),
+    vec![1, 2, 3, 4, 5, 6],
+}
 
-    assert_eq!(vec![1,2,3,4,5,6], vecmerge!(vec![1,2,3] + vec![4,5,6]));
+assert_eq! {
+    vecmerge!(vec![1, 2, 3] + vec![4, 5, 6]),
+    vec![1, 2, 3, 4, 5, 6],
+}
+
+assert_eq! {
+    vecmerge!([Some(1)]? + [None]? + [Some(3)]? + [4, 5, 6]),
+    vec![1, 3, 4, 5, 6],
+}
+
+assert_eq! {
+    vecmerge!([Some(1), Some(2), Some(3)]? + [4, 5, 6]),
+    vec![1, 2, 3, 4, 5, 6],
+}
+
+assert_eq! {
+    vecmerge!([Some(1), None, Some(3)]? + [4, 5, 6]),
+    vec![4, 5, 6],
 }
 ```
--- vecmerge-impl/src/lib.rs
+++ vecmerge-impl/src/lib.rs
@@ -1,35 +1,87 @@
 extern crate proc_macro;
 
+use proc_macro2::{Ident, Span, TokenStream};
 use proc_macro_hack::proc_macro_hack;
+use proc_quote::quote;
 
 #[proc_macro_hack]
 pub fn vecmerge(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let expr = syn::parse_macro_input!(input as syn::Expr);
-    let mut elems = Vec::new();
-    extract(expr, &mut elems);
-    let result = proc_quote::quote! {{
-        let mut v = Vec::new();
-        #(#elems)*
-        v
-    }};
-    result.into()
+    let mut visitor = Visitor::new();
+    visitor.visit(&expr);
+    let (capacity, parts) = visitor.get();
+    proc_macro::TokenStream::from(quote!({
+        let mut vec = Vec::with_capacity(#(#capacity)+*);
+        #(#parts)*
+        vec
+    }))
 }
 
-fn extract(expr: syn::Expr, elems: &mut Vec<proc_macro2::TokenStream>) {
-    match expr {
-        syn::Expr::Binary(expr) => {
-            if let syn::BinOp::Add(..) = expr.op {
-                extract(*expr.left, elems);
-                extract(*expr.right, elems);
+struct Visitor {
+    capacity: Vec<TokenStream>,
+    parts: Vec<TokenStream>,
+}
+
+impl Visitor {
+    fn new() -> Self {
+        Self {
+            capacity: Vec::new(),
+            parts: Vec::new(),
+        }
+    }
+
+    fn get(self) -> (Vec<TokenStream>, Vec<TokenStream>) {
+        (self.capacity, self.parts)
+    }
+
+    fn visit(&mut self, expr: &syn::Expr) {
+        match expr {
+            syn::Expr::Binary(ref binary_expr) => match binary_expr.op {
+                syn::BinOp::Add(..) => {
+                    self.visit(binary_expr.left.as_ref());
+                    self.visit(binary_expr.right.as_ref());
+                }
+                op => self.parts.push(quote! {
+                    compile_error!(format!("Unexpected operator {}", #op))
+                }),
+            },
+            syn::Expr::Try(ref try_expr) => match try_expr.expr.as_ref() {
+                syn::Expr::Array(array_expr) => self.visit_try_array_expr(array_expr),
+                _ => self.visit_expr(expr),
+            },
+            syn::Expr::Array(array_expr) => self.visit_array_expr(array_expr),
+            _ => self.visit_expr(expr),
+        }
+    }
+
+    fn visit_array_expr(&mut self, array_expr: &syn::ExprArray) {
+        let len = array_expr.elems.len();
+        self.capacity.push(quote!(#len));
+        for elem in array_expr.elems.iter() {
+            self.parts.push(quote! {
+                vec.push(#elem);
+            });
+        }
+    }
+
+    fn visit_try_array_expr(&mut self, try_array_expr: &syn::ExprArray) {
+        let elems = &try_array_expr.elems;
+        let names = (0..elems.len())
+            .into_iter()
+            .map(|index| Ident::new(&format!("elem_{}", index), Span::call_site()))
+            .collect::<Vec<Ident>>();
+        let pattern = quote!((#(Some(#names)),*));
+        let expr = quote!((#elems));
+        let pushes = quote!(#(vec.push(#names);)*);
+        self.parts.push(quote! {
+            if let #pattern = #expr {
+                #pushes
             }
-        }
-        syn::Expr::Array(array) => {
-            for elem in array.elems.into_iter() {
-                elems.push(proc_quote::quote!(v.push(#elem);))
-            }
-        }
-        vector => {
-            elems.push(proc_quote::quote!(v.extend(#vector);));
-        }
+        });
+    }
+
+    fn visit_expr(&mut self, expr: &syn::Expr) {
+        self.capacity.push(quote!(#expr.len()));
+        self.parts.push(quote! { vec.extend(#expr); });
     }
 }
